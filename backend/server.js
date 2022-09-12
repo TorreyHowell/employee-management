b46b73e52f0437cfd9283a3d13db2aaa10e7064d
Torrey Howell <torreythowell@gmail.com>
2022-09-12 16:57:44 -0400

    heroku

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
 const connectDB = require('./config/db')
@@ -14,6 +15,19 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
 router(app)
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+  })
+} else {
+  app.get('/', (_, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API' })
+  })
+}
 
 app.use(errorHandler)
 
